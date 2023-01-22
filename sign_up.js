d6176d10457d670dc3ab573860aceff6059edd36
sslsanjit <sslsanjit@gmail.com>
2023-01-22 12:13:37 +0100

    added empty string to usernameCheck function

--- sign_up.js
+++ sign_up.js
@@ -4,5 +4,8 @@ function usernameCheck(){
     if(usernameInput.length < 5 || usernameInput.length > 12){
         returnText = "Not Valid. Username must be 5-12 characters long!";
     }
+    else{
+        returnText =  "";
+    }
     document.getElementById("usernameValidation").innerHTML = returnText;
 }
