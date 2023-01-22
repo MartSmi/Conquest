ff2f8f2194f0c49a65157e13748087426a4024c9
sslsanjit <sslsanjit@gmail.com>
2023-01-22 13:34:09 +0100

    modifying requiredFieldCheck()

--- sign_up.js
+++ sign_up.js
@@ -25,3 +25,18 @@ function passwordCheck(){
 
     document.getElementById("passwordValidation").innerHTML = returnText;
 }
+
+function requiredFieldCheck(){
+    let requiredInputs = document.getElementsByClassName("requiredField");
+    let returnText;
+    for(let i = 0; i < requiredInputs.length; i++){
+        if(requiredInputs[i].length < 5){
+            returnText = "Required field!"
+        }
+        else{
+            returnText = "";
+        }
+        console.log("hello");
+        document.getElementsByClassName("requiredValidation").innerHTML = returnText;
+    }
+}
