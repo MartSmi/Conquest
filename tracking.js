34304c9e4119035ed4a972307da85f7d614a37bb
Martin Smith <martynas.smi@gmail.com>
2023-01-23 15:36:53 +0100

    Added all the regex

--- tracking.js
+++ tracking.js
@@ -1,13 +1,14 @@
-function toggleHiddenDiv(){
-    var hiddenDiv = document.getElementById("trackingInfo"); 
-    hiddenDiv.classList.remove("hideTrackingInfo");
+function toggleHiddenDiv() {
+  var hiddenDiv = document.getElementById("trackingInfo");
+  hiddenDiv.classList.remove("hideTrackingInfo");
 }
 
 var clicks = 0;
-document.addEventListener('click', () =>{
-    clicks++;
-    document.getElementById("info").innerHTML = "Number of mouse clicks: " + clicks;
-})
+document.addEventListener("click", () => {
+  clicks++;
+  document.getElementById("info").innerHTML =
+    "Number of mouse clicks: " + clicks;
+});
 
 var timeSpent = 0;
 /*document.addEventListener('onload',()=>{
