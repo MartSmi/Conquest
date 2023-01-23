3c0c7f30c1e7b4ce39aa83168ca5b62eee648c00
sslsanjit <sslsanjit@gmail.com>
2023-01-23 15:05:42 +0100

    added timeSpentTracker Function

--- tracking.js
+++ tracking.js
@@ -1,16 +1,33 @@
 function toggleHiddenDiv(){
     var hiddenDiv = document.getElementById("trackingInfo"); 
     hiddenDiv.classList.remove("hideTrackingInfo");
+    addClicks();
+    timeSpentTracker();
 }
 
 var clicks = 0;
 document.addEventListener('click', () =>{
     clicks++;
-    document.getElementById("info").innerHTML = "Number of mouse clicks: " + clicks;
 })
 
-var timeSpent = 0;
-/*document.addEventListener('onload',()=>{
-    var startTime = 
-})
-*/
+function addClicks(){
+    document.getElementById("info").innerHTML = 
+    "Number of mouse clicks: " + clicks;
+}
+
+var startTime;
+window.addEventListener('load',()=>{
+    startTime = new Date().getTime();
+    console.log(startTime);
+});
+
+function timeSpentTracker(){
+    var currentTime = new Date().getTime();
+    var timeSpent = currentTime - startTime;
+    var minutes = timeSpent/60000;
+    var seconds = Math.floor((minutes - Math.floor(minutes))*60);
+    document.getElementById("info").innerHTML += 
+    "<br/> Time spent: " + Math.floor(minutes) + " minutes " 
+    + Math.floor(seconds) + " seconds<br/>";
+}
+
